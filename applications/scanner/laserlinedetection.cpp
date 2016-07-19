8c2b651d06127c98f6271d2d8caa1f2fb2ab774a
Semyon Kutuzov <computer016@gmail.com>
2016-07-19 21:58:44 +0300

    Separate channels preview

---
+++ applications/scanner/laserlinedetection.cpp
@@ -0,0 +1,6 @@
+#include "laserlinedetection.h"
+
+LaserLineDetection::LaserLineDetection()
+{
+
+}
---
+++ applications/scanner/laserlinedetection.h
@@ -0,0 +1,11 @@
+#ifndef LASERLINEDETECTION_H
+#define LASERLINEDETECTION_H
+
+
+class LaserLineDetection
+{
+public:
+    LaserLineDetection();
+};
+
+#endif // LASERLINEDETECTION_H
