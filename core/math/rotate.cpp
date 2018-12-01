e7a469a32ea270ed5a110634b593e9a697a2629f
Alexander.A.Pimenov <Aleksandr.Pimenov@lanit-tercom.com>
2018-12-01 23:28:31 +0300

    Refactor Lancoz rotation

--- core/math/rotate.cpp
+++ core/math/rotate.cpp
@@ -1,144 +1,100 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
-#include <cmath>
 #include "core/buffers/rgb24/rgb24Buffer.h"
+#include "core/utils/global.h"
+#include "core/math/rotate.h"
 
 namespace corecvs {
 
+inline double sinc(double x) {
+    x = (x * M_PI);
+    return sin(x) / x;
+}
 
-    double pi = 3.14159265358979323846;
-
-    inline double sinc(double x) {
-        x = (x * pi);
-        return sin(x) / x;
+inline double LanczosFilter(double x, double lanczos_size) {
+    if (std::abs(x) < 0.0001) {
+        return 1.0;
+    }
+    if (std::abs(x) < lanczos_size) {
+        return (sinc(x) * sinc(x / lanczos_size));
     }
 
-    inline double LanczosFilter(double x, double lanczos_size) {
-        if (std::abs(x) < 0.0001) {
-            return 1.0;
-        }
-        if (std::abs(x) < lanczos_size) {
-            return (sinc(x) * sinc(x / lanczos_size));
-        }
+    return 0.0;
+}
 
-        return 0.0;
-    }
+inline double maxOf4(double d1, double d2, double d3, double d4){
+    return std::max(d1, std::max(d2, std::max(d3, d4)));
+}
 
-    inline double maxOf4(double d1, double d2, double d3, double d4){
-        return std::max(d1, std::max(d2, std::max(d3, d4)));
-    }
+inline double minOf4(double d1, double d2, double d3, double d4){
+    return std::min(d1, std::min(d2, std::min(d3, d4)));
+}
 
-    inline double minOf4(double d1, double d2, double d3, double d4){
-        return std::min(d1, std::min(d2, std::min(d3, d4)));
-    }
+RGB24Buffer* RotateHelper::rotateWithLancoz(double angle, const RGB24Buffer *rgb24buffer, int newH, int newW, double lanczos_size) {
 
-    RGB24Buffer* rotate_with_lanczoc_filter(double angle, RGB24Buffer rgb24buffer, double lanczos_size = 3.0) {
-
-        float sin_angle = sin(pi / 180 * angle);
-        float cos_angle = cos(pi / 180 * angle);
-
-        double x_center = rgb24buffer.getW() / 2;
-        double y_center = rgb24buffer.getH() / 2;
-
-        double x;
-        double y;
-
-        x = -x_center;
-        y = -y_center;
-
-        double x1 = x * cos_angle - y * sin_angle;
-        double y1 = x * sin_angle + y * cos_angle;
-
-        x = -x_center;
-        y = y_center;
-        double x2 = x * cos_angle - y * sin_angle;
-        double y2 = x * sin_angle + y * cos_angle;
-
-        x = x_center;
-        y = y_center;
-
-        double x3 = x * cos_angle - y * sin_angle;
-        double y3 = x * sin_angle + y * cos_angle;
-
-        x = x_center;
-        y = -y_center;
-
-        double x4 = x * cos_angle - y * sin_angle;
-        double y4 = x * sin_angle + y * cos_angle;
-
-        double x_new_top = maxOf4(x1, x2, x3, x4);
-        double y_new_top = maxOf4(y1, y2, y3, y4);
-
-        double x_new_bottom = minOf4(x1, x2, x3, x4);
-        double y_new_bottom = minOf4(y1, y2, y3, y4);
-
-        int new_width = x_new_top - x_new_bottom;
-        int new_height = y_new_top - y_new_bottom;
-
-         RGB24Buffer *rotated_image = new RGB24Buffer(new_height, new_width);
+    Matrix33 operation = // Matrix33::ShiftProj(rgb24buffer->w / 2, rgb24buffer->h / 2)
+                         Matrix33::ShiftProj(newW / 2, newH / 2)
+                       * Matrix33::RotationZ(angle)
+                       * Matrix33::ShiftProj(-rgb24buffer->w / 2, -rgb24buffer->h / 2);
+    operation.invert();
+    RGB24Buffer *rotated = new RGB24Buffer(newH, newW);
 
 
-        for (int i = 0; i < new_width; i++) {
-            x = i + x_new_bottom;
+    for (int i = 0; i < rotated->w; i++) {
+        for (int j = 0; j < rotated->h; j++) {
 
-            for (int j = 0; j < new_height; j++) {
+            Vector2dd target = operation * Vector2dd(i, j);
 
-                y = j + y_new_bottom;
+            int x_src = target.x();
+            int y_src = target.y();
 
-                double x_within = x * cos_angle + y * sin_angle + x_center;
-                double y_within = -x * sin_angle + y * cos_angle + y_center;
+            double weight = 0.0;
+            Vector3dd colorSum(0.0);
 
-                int x_src = x_within;
-                int y_src = y_within;
+            for (int i_sample = x_src - lanczos_size + 1; i_sample <= x_src + lanczos_size; ++i_sample) {
+                for (int j_sample = y_src - lanczos_size + 1; j_sample <= y_src + lanczos_size; ++j_sample) {
 
-                double weight = 0.0;
-                int red = 0;
-                int green = 0;
-                int blue = 0;
-                for (int i_sample = x_src - lanczos_size + 1; i_sample <= x_src + lanczos_size; ++i_sample) {
-                    for (int j_sample = y_src - lanczos_size + 1; j_sample <= y_src + lanczos_size; ++j_sample) {
+                    double lanc_val = LanczosFilter(target.x() - i_sample, lanczos_size) * LanczosFilter(target.y() - j_sample, lanczos_size);
+                    weight += lanc_val;
 
-                        double lanc_val = LanczosFilter(x_within - i_sample, lanczos_size) * LanczosFilter(y_within - j_sample, lanczos_size);
-                        weight += lanc_val;
-                        if (i_sample >= 0 && i_sample < rgb24buffer.getW() && j_sample >= 0 && j_sample < rgb24buffer.getH()) {
-
-                            red += rgb24buffer.element(j_sample, i_sample).r() * lanc_val,
-                            green += rgb24buffer.element(j_sample, i_sample).g() * lanc_val,
-                            blue += rgb24buffer.element(j_sample, i_sample).b() * lanc_val;
-
-                        }
+                    if (rgb24buffer->isValidCoord(j_sample, i_sample)) {
+                        colorSum += rgb24buffer->element(j_sample, i_sample).toDouble() * lanc_val;
                     }
                 }
-
-                red /= weight;
-                green /= weight;
-                blue /= weight;
-
-                red = (red > 255) ? 255 : red;
-                red = (red < 0) ? 0 : red;
-
-                green = (green > 255) ? 255 : green;
-                green = (green < 0) ? 0 : green;
-
-                blue = (blue > 255) ? 255 : blue;
-                blue = (blue < 0) ? 0 : blue;
-
-                rotated_image->drawPixel(i, j, RGBColor(red, green, blue));
             }
 
+            colorSum /= weight;
+
+            int red   = clamp(fround(colorSum.x()), 0, 255);
+            int green = clamp(fround(colorSum.y()), 0, 255);
+            int blue  = clamp(fround(colorSum.z()), 0, 255);
+
+            rotated->element(j, i) = RGBColor(red, green, blue);
         }
 
-        return rotated_image;
     }
+
+    return rotated;
 }
-/*
-int main(){
-    rotate_with_lanczoc_filter(180, "image.bmp");
-    int wait;
-    std::cin >> wait;
-    return 0;
-}*/
+
+Rectangled RotateHelper::getTargetSize(const Rectangled &input, const Matrix33 &forwardTransform)
+{
+    vector<Vector2dd> points = input.getPoints();
+    Rectangled target;
+    for (const Vector2dd &p : points)
+    {
+        target.extendToFit(p);
+    }
+    return target;
+}
+
+Rectangled RotateHelper::getTargetSize(const RGB24Buffer &input, const Matrix33 &forwardTransform)
+{
+    return getTargetSize(Rectangled(0,0, input.w, input.h), forwardTransform);
+}
+
+}
 
 
 
--- test-core/rotation/main_test_rotation_lanzcos.cpp
+++ test-core/rotation/main_test_rotation_lanzcos.cpp
@@ -19,24 +19,93 @@
 #include "core/buffers/rgb24/abstractPainter.h"
 #include "core/buffers/bufferFactory.h"
 #include "core/math/rotate.h"
-#include <ctime>
 #include <core/utils/preciseTimer.h>
+#include <core/stats/calculationStats.h>
+#include <core/geometry/raytrace/perlinNoise.h>
 
 using namespace corecvs;
 
-TEST(RotationTest, testRotate15angle)
+RGB24Buffer *getNoiseImage(int h, int w)
 {
-    RGB24Buffer *buffer = BufferFactory::getInstance()->loadRGB24Bitmap("hero.bmp");
-    BMPLoader().save("rotated_image_hero_15.bmp", rotate_with_lanczoc_filter(15, buffer));
-    delete buffer;
+    RGB24Buffer *turbr = new RGB24Buffer(h, w);
+
+    SYNC_PRINT(("Initing...\n"));
+
+    PerlinNoise gen;
+
+    SYNC_PRINT(("Generating...\n"));
+
+    for (int i = 0; i < turbr->h; i++)
+    {
+        for (int j = 0; j < turbr->w; j++)
+        {
+            Vector3dd p = Vector3dd(i,j,0) / 40.0;
+            turbr->element(i,j) = RGBColor::FromDouble(gen.turbulence3d(p) * 255.0);
+        }
+    }
+    return turbr;
 }
 
+TEST(RotationTest, testRotate)
+{
+    // RGB24Buffer *buffer = BufferFactory::getInstance()->loadRGB24Bitmap("hero.bmp");
+    Statistics stats;
+
+    stats.startInterval();
+    RGB24Buffer *buffer = getNoiseImage(768, 1024);
+    stats.resetInterval("Generating image");
+    RGB24Buffer *rotated = RotateHelper::rotateWithLancoz(degToRad(15), buffer, 1006, 1187);
+    stats.resetInterval("Rotating image");
+    BMPLoader().save("rotated_15.bmp", rotated);
+    stats.endInterval("Saving image");
+
+    delete_safe(rotated);
+    delete_safe(buffer);
+
+    BaseTimeStatisticsCollector(stats).printAdvanced();
+}
+
+TEST(RotationTest, quality)
+{
+
+    Statistics stats;
+
+    stats.startInterval();
+    RGB24Buffer *buffer = getNoiseImage(768, 1024);
+    stats.resetInterval("Generating image");
+    RGB24Buffer *rotated   = RotateHelper::rotateWithLancoz(degToRad(15), buffer, 1006, 1187, 2);
+    stats.resetInterval("Rotating image");
+    RGB24Buffer *rotatedback = RotateHelper::rotateWithLancoz(degToRad(-15), rotated, 768, 1024, 2);
+    stats.resetInterval("Rotating back");
+
+    int value = 0;
+    /*Lets cut the boundary to make comparison only inside */
+    RGB24Buffer in  = buffer     ->createView<RGB24Buffer>(2,2, buffer->h - 4, buffer->w - 4);
+    RGB24Buffer out = rotatedback->createView<RGB24Buffer>(2,2, buffer->h - 4, buffer->w - 4);
+
+
+    RGB24Buffer *diff = RGB24Buffer::diff(&in, &out, &value);
+    diff->mapOperationElementwize([](const RGBColor &in){return RGBColor::FromFloat(in.toFloat() * 200.0);});
+
+    BMPLoader().save("rotated_diff.bmp", diff);
+
+    cout << "Error value:" << value << endl;
+    cout << "Per pixel  :" << (double) value / in.numElements() << endl;
+    //cout << "Steps      :" << (double) value / in.numElements() * 256.0 << endl;
+
+    delete_safe(diff);
+    delete_safe(rotatedback);
+    delete_safe(rotated);
+    delete_safe(buffer);
+
+    BaseTimeStatisticsCollector(stats).printAdvanced();
+}
+
+#if 0
 TEST(RotationTest, testRotateMinus15angle)
 {
     RGB24Buffer *buffer = BufferFactory::getInstance()->loadRGB24Bitmap("hero.bmp");
     BMPLoader().save("rotated_image_hero_minus_15.bmp", rotate_with_lanczoc_filter(-15, buffer));
     delete buffer;
 }
-
-
-
+#endif
