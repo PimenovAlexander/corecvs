41e4573d8515159d4da60aa2f075d427d3d70a4e
NIKOLAY <39766457+kola1197@users.noreply.github.com>
2018-12-18 04:33:26 +0300

    Update physicsMainWidget.h

--- applications/physics/physicsMainWidget.h
+++ applications/physics/physicsMainWidget.h
@@ -7,6 +7,7 @@
 #include <QtSerialPort/QSerialPort>
 #include <QtSerialPort/QSerialPortInfo>
 #include <linux/joystick.h>
+#include <fcntl.h>
 
 namespace Ui {
 class PhysicsMainWidget;
@@ -15,16 +16,18 @@ class PhysicsMainWidget;
 class PhysicsMainWidget : public QWidget
 {
     Q_OBJECT
-
-public:
+ public:
     explicit PhysicsMainWidget(QWidget *parent = 0);
     QSerialPort serialPort;
     ~PhysicsMainWidget();
      QByteArray *FlyCommandWriteData;
      QByteArray GetDataFromStics();
-
+     int Current_mode=0;
      bool RealModeActive = false;
      bool VirtualModeActive = false;
+     struct axis_state {       short x, y;    };
+
+     void disconnect_from_copter();
 public slots:
     void keepAlive();
 private slots:
@@ -41,6 +44,8 @@ private slots:
     void STOP();
     void on_pushButton_2_clicked();
 
+    void on_comboBox_currentTextChanged(const QString &arg1);
+
 private:
     Ui::PhysicsMainWidget *ui;
     int yaw_value;
@@ -52,6 +57,8 @@ private:
     int sixth_CH;
     int seventh_CH;
     int eighth_CH;
+    int mid_Throttle=1350;
+
     int counter;
     int sign(int val);
     clock_t start_time;
@@ -61,9 +68,26 @@ private:
     bool bind=false;
     bool Arming=false;
     bool startFly=false;   //to set mid throttle after arming
-    void SendOurValues(std::vector<uint8_t> OurValues);
 
+    bool rt_pressed=false;
+    bool lt_pressed=false;
+
+    void SendOurValues(std::vector<uint8_t> OurValues);
+    void usial_buttons(js_event event);
+    void usial_sticks(js_event event);
+
+    void inertial_buttons(js_event event);
+    void inertial_sticks(js_event event);
+
+    void casual_buttons(js_event event);
+    void casual_sticks(js_event event);
+
+    struct axis_state axes[3] ;
+
+    void Start_arming(bool pressed);
     ClientSender VirtualSender;
+
+    //size_t get_button_count(int fd);
 };
 
 #endif // PHYSICSMAINWIDGET_H
