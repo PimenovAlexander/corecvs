49d3a525ef060cba3213a67f405556b2d092a084
Mark Kovalev <restonich@gmail.com>
2018-12-28 18:33:26 +0300

    Snapshot

--- test-core/BSPRenderTest/BSPRenderTest.cpp
+++ test-core/BSPRenderTest/BSPRenderTest.cpp
@@ -37,160 +37,62 @@ TEST(BSPRender, levelDraw)
 
     corecvs::Polygon p;
     RGB24Buffer *buffer = new RGB24Buffer(h, w, RGBColor::Black());
+    RGB24Buffer *buffer1 = new RGB24Buffer(h, w, RGBColor::Black());
 
-    p.push_back(Vector2dd(0, 0));
+    p.push_back(Vector2dd(59, 59));
 
     p.push_back(Vector2dd(299, 59));
     p.push_back(Vector2dd(239, 239));
     p.push_back(Vector2dd(479, 299));
     p.push_back(Vector2dd(479, 59));
 
-    p.push_back(Vector2dd(719, 0));
-    p.push_back(Vector2dd(719, 479));
-    p.push_back(Vector2dd(539, 719));
+    p.push_back(Vector2dd(659, 59));
+    p.push_back(Vector2dd(659, 479));
+    p.push_back(Vector2dd(539, 659));
 
-    p.push_back(Vector2dd(479, 719));
+    p.push_back(Vector2dd(479, 659));
     p.push_back(Vector2dd(479, 479));
     p.push_back(Vector2dd(299, 419));
     p.push_back(Vector2dd(299, 599));
 
-    p.push_back(Vector2dd(0, 719));
+    p.push_back(Vector2dd(59, 659));
 
     AbstractPainter<RGB24Buffer> painter(buffer);
     painter.drawPolygon(p, RGBColor::White());
+
     BMPLoader().save("levelPolygon.bmp", buffer);
 
+//    Ray2d ray(Vector2dd(100, 719), Vector2dd(200, 0));
+//    cout << "ray.getStart() = " << ray.getStart() << endl;
+//    cout << "ray.getEnd() = " << ray.getEnd() << endl;
+//    buffer1->drawLine(ray.getStart(), ray.getEnd(), RGBColor::Yellow());
+//    double t1, t2;
+//    ray.clip<Polygon>(p, t1, t2);
 
+//    Vector2dd p1 = ray.getPoint(t1);
+//    Vector2dd p2 = ray.getPoint(t2);
 
-//    PolygonPointIterator it(p);
-//    for (Vector2d<int> point : it) {
-//        //cout << point << endl;
-//        ASSERT_TRUE(buffer->isValidCoord(point));
-//        buffer->element(point) += RGBColor::Red();
-//    }
+//    cout << "t1 = " << t1 << endl;
+//    cout << "t2 = " << t2 << endl;
+//    cout << "p1 = " << p1 << endl;
+//    cout << "p2 = " << p2 << endl;
 
-//    BMPLoader().save("polygon2.bmp", buffer);
+//    buffer1->drawLine(p1, p2, t1 > t2 ? RGBColor::Red() : RGBColor::Green());
+//    AbstractPainter<RGB24Buffer> painter1(buffer1);
+//    painter1.drawPolygon(p, RGBColor::White());
+
+//    BMPLoader().save("levelPolygon1.bmp", buffer1);
+
+//    Ray2d ray = p.getRay(2);
+//    Vector2dd p1 = ray.getStart();
+//    Vector2dd p2 = ray.getEnd();
+//    printf("Start: %f, %f\n"
+//           "End: %f, %f\n",
+//           p1.x(), p1.y(),
+//           p2.x(), p2.y());
+//    buffer->drawLine(p1, p2, RGBColor::Red());
+//    BMPLoader().save("levelPolygon1.bmp", buffer);
+
     delete_safe(buffer);
-}
-
-
-TEST(Draw, testPoly)
-{
-    int h = 1024;
-    int w = 720;
-
-    RGB24Buffer *buffer = new RGB24Buffer(h, w, RGBColor::Black());
-    Vector2dd center(w / 2.0, h / 2.0);
-
-    AbstractPainter<RGB24Buffer> painter(buffer);
-
-    corecvs::Polygon p;
-    for (int i = 0; i < 7; i++) {
-        p.push_back(Vector2dd::FromPolar((2 * M_PI / 7.0) * i, 100.0) + center);
-    }
-
-    cout << p << std::endl;
-
-    painter.drawPolygon(p, RGBColor::Blue());
-
-    PolygonPointIterator it(p);
-    for (Vector2d<int> p: it) {
-        if (buffer->isValidCoord(p)) {
-            buffer->element(p) += RGBColor(140, 140, 0);
-        }
-    }
-
-    BMPLoader().save("polygon.bmp", buffer);
-    delete_safe(buffer);
-}
-
-
-TEST(Draw, testPoly1)
-{
-    int h = 240;
-    int w = 240;
-
-    RGB24Buffer *buffer = new RGB24Buffer(h, w, RGBColor::Black());
-    Vector2dd center(buffer->h / 12, buffer->h / 12);
-    double radius = center.x() / 10.0 * 9.0;
-
-    AbstractPainter<RGB24Buffer> painter(buffer);
-
-    for (int pi = 0; pi < 3; pi ++)
-    {
-        for (int pj = 0; pj < 3; pj ++)
-        {
-            corecvs::Polygon p;
-            int count = pi * 3 + pj + 3;
-            for (int i = 0; i < count; i++) {
-                p.push_back(Vector2dd::FromPolar((2 * M_PI / count) * i, radius) + center * Vector2dd(pi * 2 + 1, pj * 2 + 1));
-            }
-
-           // cout << p << std::endl;
-
-            painter.drawPolygon(p, RGBColor::Blue());
-
-            PolygonSpanIterator it(p);
-            for (HLineSpanInt l: it)
-            {
-                for (Vector2d<int> point : l) {
-                    if (buffer->isValidCoord(point)) {
-                        buffer->element(point) += RGBColor(90, 90, 0);
-                    }
-                }
-            }
-        }
-    }
-
-    for (int pi = 0; pi < 3; pi ++)
-    {
-        for (int pj = 0; pj < 3; pj ++)
-        {
-            corecvs::Polygon p;
-            int count = pi * 3 + pj + 3;
-            for (int i = 0; i < count; i++) {
-                p.push_back(Vector2dd::FromPolar(-(2 * M_PI / count) * i, radius) + center * Vector2dd(pi * 2 + 1, pj * 2 + 1) + Vector2dd(0.0, buffer->h / 2));
-            }
-
-           // cout << p << std::endl;
-
-            painter.drawPolygon(p, RGBColor::Green());
-
-            PolygonSpanIterator it(p);
-            for (HLineSpanInt l: it)
-            {
-//                cout << l << endl;
-                for (Vector2d<int> point : l) {
-                    if (buffer->isValidCoord(point)) {
-                        buffer->element(point) += RGBColor(0, 70, 0);
-                    }
-                }
-            }
-        }
-    }
-
-    for (int pi = 0; pi < 3; pi ++)
-    {
-        for (int pj = 0; pj < 3; pj ++)
-        {
-            corecvs::Polygon p;
-            int count = pi * 3 + pj + 3;
-            for (int i = 0; i < count; i++) {
-                p.push_back(Vector2dd::FromPolar(-(2 * M_PI / count) * i, radius) + center * Vector2dd(pi * 2 + 1, pj * 2 + 1) + Vector2dd(buffer->w / 2, 0.0));
-            }
-
-            //cout << p << std::endl;
-
-            painter.drawPolygon(p, RGBColor::Green());
-
-            PolygonPointIterator it(p);
-            for (Vector2d<int> point : it) {
-                    buffer->element(point) += RGBColor(0, 70, 0);
-            }
-        }
-    }
-
-
-    BMPLoader().save("polygon1.bmp", buffer);
-    delete_safe(buffer);
+    delete_safe(buffer1);
 }
--- test-core/BSPRenderer/BSPRenderer.h
+++ test-core/BSPRenderer/BSPRenderer.h
@@ -10,14 +10,77 @@
 #ifndef BSPRENDERER_H
 #define BSPRENDERER_H
 
+#include "core/geometry/polygons.h"
+#include "core/geometry/line.h"
+#include <iterator>
+
 namespace corecvs {
 
-class BSPRenderer {
-public:
-    BSPRenderer();
+//template<typename RealType, typename GeometryType, class SeparatorType>
+//class BSPTreeNodeBase {
 
 
+//};
+
+class BSPTree2d {
+    Line2d               partition;
+    std::vector<Ray2d>   coincidentEdges;
+    BSPTree2d            *front,
+                         *back;
+    BSPTree2d() {
+
+    }
+
+    void BSPDivide(BSPTree2d *tree, std::vector<Ray2d> edges)
+    {
+        std::vector<Ray2d>::iterator edgesIt = edges.begin();
+
+        if (edgesIt == edges.end())
+            return;
+        Ray2d *root = &(*edgesIt);
+
+        tree->partition = Line2d(*root);
+        tree->coincidentEdges.push_back(*root);
+        std::vector<Ray2d> frontEdges,
+                           backEdges;
+
+        ++edgesIt;
+        while (edgesIt != edges.end()) {
+            Ray2d *curEdge = &(*edgesIt);
+            int result = classifyEdge(tree->partition, curEdge);
+
+            switch (result) {
+                case COINCIDENT:
+                    tree->coincidentEdges.push_back(*curEdge);
+                    break;
+                case IN_BACK:
+                    backEdges.push_back(*curEdge);
+                    break;
+                case IN_FRONT:
+                    frontEdges.push_back(*curEdge);
+                    break;
+                case INTERCECT:
+                    Ray2d *frontPart, *backPart;
+                    SplitRay2d(curEdge, tree->partition, frontPart, backPart);
+                    backEdges.push_back(*backPart);
+                    frontEdges.push_back(*frontPart);
+                    break;
+            }
+            ++edgesIt;
+        }
+
+        if (!frontEdges.empty()) {
+            tree->front = new BSPTree2d;
+            BSPDivide(tree->front, frontEdges);
+        }
+
+        if (!backEdges.empty()) {
+            tree->back = new BSPTree2d;
+            BSPDivide (tree->back, backEdges);
+        }
+    }
 };
+
 
 } // namespace corecvs
 
