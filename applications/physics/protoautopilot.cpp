8208023aa44a58d55c1fe7c03ed143c9def7c842
kola1197 <kola-1197@mail.ru>
2019-03-11 13:33:18 +0300

    some fixes

---
+++ applications/physics/protoautopilot.cpp
@@ -0,0 +1,6 @@
+#include "protoautopilot.h"
+
+ProtoAutoPilot::ProtoAutoPilot()
+{
+
+}
---
+++ applications/physics/protoautopilot.h
@@ -0,0 +1,11 @@
+#ifndef PROTOAUTOPILOT_H
+#define PROTOAUTOPILOT_H
+
+
+class ProtoAutoPilot
+{
+public:
+    ProtoAutoPilot();
+};
+
+#endif // PROTOAUTOPILOT_H
