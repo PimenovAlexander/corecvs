cd8a868681368d2831e3ac020eaf2dd92aefee6f
DmiitriyJarosh <dimajarosh@gmail.com>
2019-10-06 23:49:23 +0300

    path to args

--- test/focus_stack/main.cpp
+++ test/focus_stack/main.cpp
@@ -8,20 +8,22 @@
 using namespace std;
 using namespace corecvs;
 
+//Usage: ./focus_stack <path to stack> <amount of images in stack> <path to output dir>
+
 int main(int argc, char *argv[])
 {
-    if (argc != 2) {
+    if (argc != 4) {
         SYNC_PRINT(("Wrong amount of arguments!\n"));
         return 1;
     }
 
-    ImageStack * imageStack = ImageStack::loadStack("/home/adminlinux/Desktop/testStack", 4);
+    ImageStack * imageStack = ImageStack::loadStack(argv[1], std::stoi(argv[2]));
     if (imageStack == nullptr)
     {
         SYNC_PRINT(("Can't load images!\n"));
     }
     LaplacianStacking lapl;
     imageStack->focus_stack(lapl);
-    imageStack->saveMegredImage("/home/adminlinux/Desktop");
+    imageStack->saveMegredImage(argv[3]);
 }
 
