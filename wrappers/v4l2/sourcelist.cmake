273f361506fcfc6019a274d99234ee9c9224a192
janchk <janchk@gmail.com>
2019-10-27 19:13:50 +0300

    grab24 and wrapper

---
+++ wrappers/v4l2/sourcelist.cmake
@@ -0,0 +1,14 @@
+set (HDR_FILES
+    ${HDR_FILES}
+    ${CMAKE_CURRENT_LIST_DIR}/V4L2CaptureDecouple.h  
+    ${CMAKE_CURRENT_LIST_DIR}/V4L2Capture.h 
+    ${CMAKE_CURRENT_LIST_DIR}/V4L2.h
+)
+
+
+set (SRC_FILES
+    ${SRC_FILES}
+    ${CMAKE_CURRENT_LIST_DIR}/V4L2CaptureDecouple.cpp 
+    ${CMAKE_CURRENT_LIST_DIR}/V4L2Capture.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/V4L2.cpp
+)
