5415e4d79c85a72e23a970b9b6a149ac9cdebfea
jakhremchik <janchk@gmail.com>
2019-11-15 20:30:21 +0300

    That should work. In theory...

--- wrappers/apriltag_wrapper/apriltagDetector.cpp
+++ wrappers/apriltag_wrapper/apriltagDetector.cpp
@@ -5,34 +5,26 @@
 #include "apriltagDetector.h"
 
 
-apriltagDetector::apriltagDetector() {
-    params.tag_family = "tag36h11";
-    params.at_debug = true;
-    params.at_quiet = false;
-    params.at_threads = 1;
-    params.blur = 0.0;
-    params.at_refine_edges = true;
-    params.at_decimate = 2.0;
-
+ApriltagDetector::ApriltagDetector(){
     apriltag_family_t *tf = nullptr;
     at_detections = nullptr;
 
-     if ("tag36h11" == params.tag_family) {
+     if ("tag36h11" == params.tag_family()) {
         tf = tag36h11_create();
-    } else if ("tag25h9" == params.tag_family) {
+    } else if ("tag25h9" == params.tag_family()) {
         tf = tag25h9_create();
-    } else if ("tag16h5" == params.tag_family) {
+    } else if ("tag16h5" == params.tag_family()) {
         tf = tag16h5_create();
-        // circles probably wont return correct output for PatternDetection.
-    } else if ("tagCircle21h7" == params.tag_family) {
+        // circles probably won't return correct output for PatternDetection.
+    } else if ("tagCircle21h7" == params.tag_family()) {
         tf = tagCircle21h7_create();
-    } else if ("tagCircle49h12" == params.tag_family) {
+    } else if ("tagCircle49h12" == params.tag_family()) {
         tf = tagCircle49h12_create();
-    } else if ("tagStandard41h12" == params.tag_family) {
+    } else if ("tagStandard41h12" == params.tag_family()) {
         tf = tagStandard41h12_create();
-    } else if ("tagStandard52h13" == params.tag_family) {
+    } else if ("tagStandard52h13" == params.tag_family()) {
         tf = tagStandard52h13_create();
-    } else if ( "tagCustom48h12" == params.tag_family) {
+    } else if ( "tagCustom48h12" == params.tag_family()) {
         tf = tagCustom48h12_create();
     } else {
         printf("Unrecognized tag family name. Use e.g. \"tag36h11\".\n");
@@ -42,35 +34,27 @@ apriltagDetector::apriltagDetector() {
     this->td = apriltag_detector_create();
     apriltag_detector_add_family(td, tf);
 
-    this->td->debug = params.at_debug;
-    this->td->quad_decimate = params.at_decimate;
-    this->td->quad_sigma = params.blur;
-    this->td->refine_edges = params.at_refine_edges;
-    this->td->nthreads = params.at_threads;
+    this->td->debug = params.at_debug();
+    this->td->quad_decimate = params.at_decimate();
+    this->td->quad_sigma = params.blur();
+    this->td->refine_edges = params.at_refine_edges();
+    this->td->nthreads = params.at_threads();
 }
 
-apriltagDetector::~apriltagDetector() {
-    delete_safe(debug);
-//    delete_safe(gray);
+ApriltagDetector::~ApriltagDetector() {
+    delete_safe(debugBuffer);
+    delete_safe(gray);
 }
 
-int apriltagDetector::operator()() {
+int ApriltagDetector::operator()() {
     using namespace corecvs;
-    bool result = false;
-//    SYNC_PRINT(("apriltagDetector::operator(): called\n"));
-
-//    image_u8_t inputGray = {
-//            .width  = gray->w,
-//            .height = gray->h,
-//            .stride = gray->stride,
-//            .buf   = gray->data
-//    };
+    SYNC_PRINT(("apriltagDetector::operator(): called\n"));
 
     image_u8_t inputGray = {
-         .width = gray.cols,
-         .height = gray.rows,
-         .stride = gray.cols,
-         .buf = gray.data
+            .width  = gray->w,
+            .height = gray->h,
+            .stride = gray->stride,
+            .buf   = gray->data
     };
 
 
@@ -81,31 +65,25 @@ int apriltagDetector::operator()() {
 }
 
 
-bool apriltagDetector::setParameters(std::string name, const corecvs::DynamicObject &param) {
+bool ApriltagDetector::setParameters(std::string name, const corecvs::DynamicObject &param) {
     if ("params" == name) {
-//        param.copyTo(&params);
+        param.copyTo(&params);
         return  true;
     }
+    return false;
 
 }
 
-void apriltagDetector::setInputImage(corecvs::RGB24Buffer *input) {
+void ApriltagDetector::setInputImage(corecvs::RGB24Buffer *input) {
     corecvs::Statistics::startInterval(stats);
-//    delete_safe(gray);
-//    gray = input->getChannelG8(ImageChannel::GRAY);
+    delete_safe(gray);
+    gray = input->getChannelG8(ImageChannel::GRAY);
 
     corecvs::Statistics::endInterval(stats, "Setting input");
 }
 
-void apriltagDetector::setInputImageCV(const cv::Mat& input) {
 
-    corecvs::Statistics::startInterval(stats);
-    cv::cvtColor(input, gray, cv::COLOR_BGR2GRAY);
-    corecvs::Statistics::endInterval(stats, "Setting input");
-}
-
-
-void apriltagDetector::getOutput(vector<corecvs::PatternDetectorResult> &patterns) {
+void ApriltagDetector::getOutput(vector<corecvs::PatternDetectorResult> &patterns) {
     using namespace corecvs;
     Statistics::startInterval(stats);
     for (int i = 0; i < zarray_size(at_detections); i++) {
@@ -119,27 +97,48 @@ void apriltagDetector::getOutput(vector<corecvs::PatternDetectorResult> &pattern
         result.mId = det->id;
         patterns.emplace_back(result);
     }
-    if (params.at_debug) {
+    if (params.at_debug()) {
+        delete_safe(debugBuffer);
+        debugBuffer = new RGB24Buffer(input);
+        for (const auto &result : patterns) {
+            Vector2dd a (result.mPosition.mX, result.mPosition.mY);
+            Vector2dd b (result.mOrtX.mX, result.mOrtX.mY);
+            Vector2dd c (result.mOrtY.mX, result.mOrtY.mY);
+            Vector2dd d (result.mUnityPoint.mX, result.mUnityPoint.mY);
 
+            RGBColor color = RGBColor::parula((double)result.mId/patterns.size());
+            debugBuffer->drawLine(a, b, color);
+            debugBuffer->drawLine(b, c, color);
+            debugBuffer->drawLine(c, d, color);
+            debugBuffer->drawLine(d, a, color);
 
+            AbstractPainter<RGB24Buffer> p(debugBuffer);
+            p.drawFormat((result.mOrtX.mX + result.mOrtY.mX)/2, (result.mOrtX.mY + result.mOrtY.mX)/2, color, 2, "%d", result.mId);
+        }
 
-//        delete_safe(debugBuffer)
     }
-
 
     Statistics::endInterval(stats, "Getting output");
 }
 
-std::vector<std::string> apriltagDetector::debugBuffers() const {
+std::vector<std::string> ApriltagDetector::debugBuffers() const {
+    if (debugBuffer != nullptr )
+        return std::vector<std::string>({"debug"});
+
     return std::vector<std::string>();
 }
 
-corecvs::RGB24Buffer *apriltagDetector::getDebugBuffer(const std::string &name) const {
+corecvs::RGB24Buffer *ApriltagDetector::getDebugBuffer(const std::string &name) const {
+    if ("debug" == name) {
+        return new corecvs::RGB24Buffer(debugBuffer);
+    }
     return nullptr;
 }
 
-std::map<std::string, corecvs::DynamicObject> apriltagDetector::getParameters() {
-    return std::map<std::string, corecvs::DynamicObject>();
+std::map<std::string, corecvs::DynamicObject> ApriltagDetector::getParameters() {
+    std::map<std::string, corecvs::DynamicObject> dparams;
+    dparams.emplace("params", corecvs::DynamicObject(&params));
+    return dparams;
 }
 
 
--- wrappers/apriltag_wrapper/sourcelist.cmake
+++ wrappers/apriltag_wrapper/sourcelist.cmake
@@ -1,13 +1,14 @@
 set (HDR_FILES
         ${HDR_FILES}
         ${CMAKE_CURRENT_LIST_DIR}/apriltagDetector.h
-)
-
+        ${CMAKE_CURRENT_LIST_DIR}/generated/apriltagParameters.h
+        )
 
 set (SRC_FILES
         ${SRC_FILES}
         ${CMAKE_CURRENT_LIST_DIR}/apriltagDetector.cpp
-)
+        ${CMAKE_CURRENT_LIST_DIR}/generated/apriltagParameters.cpp
+        )
 
 
 include_directories(${CMAKE_CURRENT_LIST_DIR})
