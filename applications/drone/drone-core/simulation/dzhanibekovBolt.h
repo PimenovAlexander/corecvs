9e20c99ad6762cb1691168b81528098cbd863bf9
Alexander.A.Pimenov <Aleksandr.Pimenov@lanit-tercom.com>
2019-11-24 17:21:51 +0300

    Adding missing files

---
+++ applications/drone/drone-core/simulation/dzhanibekovBolt.h
@@ -0,0 +1,38 @@
+#ifndef JANIBEKOVSBOLT_H
+#define JANIBEKOVSBOLT_H
+#include <physMainObject.h>
+#include "core/geometry/mesh3d.h"
+#include <copterInputs.h>
+#include <vector>
+
+#include <core/cameracalibration/cameraModel.h>
+#include "core/cameracalibration/calibrationDrawHelpers.h"
+#include "core/math/affine.h"
+#include "core/math/vector/vector3d.h"
+
+#include "physSphere.h"
+
+class DzhanibekovBolt : public PhysMainObject
+{
+
+public:
+    DzhanibekovBolt(double arm = 0.01, double mass = 6);
+
+    /* Not owned */
+    PhysSphere *centralSphere;
+    /* Not owned */
+    std::vector<PhysSphere *> partsOfSystem;
+
+    void drawMyself(Mesh3D &mesh);
+    Affine3DQ getTransform();
+    void physicsTick(double deltaT);
+    virtual void tick(double deltaT) override;
+    void drawMyself(Mesh3DDecorated &mesh);
+
+    Mesh3DDecorated *worldMesh = NULL;
+    void drawForces(Mesh3D &mesh);
+
+    virtual ~DzhanibekovBolt() override {}
+};
+
+#endif // JANIBEKOVSBOLT_H
