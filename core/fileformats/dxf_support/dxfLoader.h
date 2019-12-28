84011fd07da124934988593954adf6fae589f2f1
Vladislav76 <vladislav.myasnikov@bk.ru>
2019-12-29 00:41:16 +0300

    Add comments to DxfLoader

--- core/fileformats/dxf_support/dxfLoader.h
+++ core/fileformats/dxf_support/dxfLoader.h
@@ -17,6 +17,40 @@
 
 namespace corecvs {
 
+/**
+ * DxfLoader is intended to load and represent DXF file into special structure for further drawing.
+ *
+ * Brief description of DxfLoader's work:
+ * 1. File is loaded by sent string name.
+ * 2. In a loop, all pairs (group code, value) are extracted.
+ * 3. For each pair, depending on the group code, one of the following actions is performed:
+ *    3.1. if it's a separation code, a new entity is created and transferred to the DxfBuilder or a sub-entity is created (e.g. a vertex in a polyline)
+ *    3.2. a new system variable is set to the DxfBuilder
+ *    3.3. the pair is stored in a 'values' map
+ * 4. In the end, when all pairs were processed, DxfDrawing is created by DxfBuilder.
+ *
+ * DxfBuilder is helper-class for creating DxfDrawing.
+ *
+ * DxfDrawing is part representation of DXF file content.
+ * It contains graphic entities that are associated with a certain layer and, optionally, are tied to a certain block.
+ * Also this class contains some non-graphic objects (e.g. line types).
+ * Several parameters can be set for the DxfDrawing for further display (e.g. paddings and scaling factor).
+ * The class can return RGB24Buffer with visible entities.
+ *
+ * DxfEntity is abstract graphic DXF-entity.
+ * To add a new entity, you need to inherit from DxfEntity and implement three methods:
+ * 1. print          --- to print main information about the entity
+ * 2. draw           --- to draw the entity
+ * 3. getBoundingBox --- to calculate the bounding box
+ * Also you need to inherit from DxfEntityData in order to add entity-specific properties.
+ *
+ * DxfObject is abstract non-graphic DXF-object.
+ * To add a new object, you need to inherit from DxfObject and implement one method:
+ * 1. print          --- to print main information about the object
+ * Also you need to inherit from DxfObjectData in order to add object-specific properties.
+ * 
+ * DxfCodes contains DXF-specific codes and some helpful functions.
+ */
 class DxfLoader {
 public:
     explicit DxfLoader(DxfBuilder &dxfBuilder)
