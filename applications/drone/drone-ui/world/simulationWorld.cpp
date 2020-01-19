47c229f8cbddb366d3febe4826af1b2edc35e14c
Alexander.A.Pimenov <Aleksandr.Pimenov@lanit-tercom.com>
2020-01-19 03:09:49 +0300

    ASAP Removing code

---
+++ applications/drone/drone-ui/world/simulationWorld.cpp
@@ -0,0 +1,6 @@
+#include "simulationWorld.h"
+
+SimulationWorld::SimulationWorld()
+{
+
+}
---
+++ applications/drone/drone-ui/world/simulationWorld.h
@@ -0,0 +1,11 @@
+#ifndef SIMULATIONWORLD_H
+#define SIMULATIONWORLD_H
+
+
+class SimulationWorld
+{
+public:
+    SimulationWorld();
+};
+
+#endif // SIMULATIONWORLD_H
