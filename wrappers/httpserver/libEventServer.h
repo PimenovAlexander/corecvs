5b66e1d86b2f663d42f1c1abe53d4bbd67b67a37
Darderion <Eobard1SA@gmail.com>
2020-08-18 16:57:53 +0300

    Moved server's methods' implementation to a cpp file

--- wrappers/httpserver/libEventServer.h
+++ wrappers/httpserver/libEventServer.h
@@ -38,53 +38,10 @@ public:
         options.verbose = 0;
     }
 
-    /**
-     * Initialize libEventServer
-     * @return 0 on success, error code otherwise
-     */
-    int setup() {
-        // Creating EVENT_BASE object, which is required for almost all of LibEvent's functionality
-
-
-        if (!(base = event_base_new())) {
-            std::cerr << "Event_base_new failed" << std::endl;
-            return -1;
-        }
-
-        // Creating EVENT_HTTP object, which is responsible for routing as well as serving http content
-        server = evhttp_new(base);
-        if (evhttp_bind_socket(server, options.addr, options.port)) {
-            std::cerr << "Binding failed" << std::endl;
-            return -2;
-        }
-        return 0;
-    }
-
-    /**
-     * Set a callback for a specified URI
-     * @param path the path for which to invoke the callback
-     * @param callback the callback function that gets invoked on specified URI
-     * @return 0 on success, -1 if the callback existed already, -2 on failure
-     */
-    int set_callback(const char *path, void (*callback)(evhttp_request *, void *)) {
-        return evhttp_set_cb(server, path, callback, nullptr);
-    }
-
-    /**
-     * Set default callback
-     * Sets a callback for all requests that weren't match to any of the existing callbacks
-     * @param callback the callback function that gets invoked by default if no other callback matches request
-     */
-    void set_default_callback(void (*callback)(evhttp_request *, void *)) {
-        evhttp_set_gencb(server, callback, nullptr);
-    }
-
-    /**
-     * Process all pending requests
-     */
-    void process_requests() {
-        event_base_loop(base, EVLOOP_NONBLOCK);
-    }
+    int setup();
+    int set_callback(const char *path, void (*callback)(evhttp_request *, void *));
+    void set_default_callback(void (*callback)(evhttp_request *, void *));
+    void process_requests();
 };
 
 #endif //LIBEVENTAPP_LIBEVENTSERVER_H
